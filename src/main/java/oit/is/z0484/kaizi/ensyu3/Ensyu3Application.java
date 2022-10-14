8332d323b8722634d6c436b7de38c69457057581
e1b20038 <e1b20038@oit.ac.jp>
2022-10-14 14:19:15 +0900

    initialize

---
+++ src/main/java/oit/is/z0484/kaizi/ensyu3/Ensyu3Application.java
@@ -0,0 +1,13 @@
+package oit.is.z0484.kaizi.ensyu3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ensyu3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ensyu3Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0484/kaizi/ensyu3/Ensyu3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0484.kaizi.ensyu3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ensyu3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
